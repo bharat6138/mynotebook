ec47033606f1d4324ff64c9bc995f0e1afb70126
bharat6138 <43534123+bharat6138@users.noreply.github.com>
2022-03-22 19:55:20 +0530

    add data in database

---
+++ models/Notes.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const NotesSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  tag: {
+    type: String,
+    default:"General"
+  },
+  date: {
+    type: Date,
+    required: Date.now
+  }
+});
+
+
+module.exports = mongoose.model("notes", NotesSchema)
